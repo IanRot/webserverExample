ad27afb5fbb8d0fd3b8bca59dd870b3b292d1f55
Ian Rodriguez <ianrodriguez0201@gmail.com>
2026-02-24 09:20:18 -0400

    segundo commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,24 @@
 import express from "express";
+import path from "path";    
+import { fileURLToPath } from "url";
 
-const port = process.env?.port || 3000;
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename)
+
+const port = process.env.port || 3000;
 const app = express()
 
-app.get("/", (req, res) => {
-    return res.json({message: "Hola Mundo"})
-})
+app.use(express.static(path.join(__dirname, "../public")));
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.get ("/about", (req,res) => {
+    res.render("about");
+});
+
+app.get ("/", (req,res) => {
+    res.render("index");
+});
 
 app.listen(port, () => {
     console.log(`Running on port ${port}`)
